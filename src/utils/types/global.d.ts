ec9dd399da618732dcaf1422add48cb2f3315050
Evelina Esin <isjsfidjdh@gmail.com>
2025-04-24 07:49:28 -0700

    Delete src directory

--- src/utils/types/global.d.ts
+++
@@ -1,7 +0,0 @@
-type ConstructorType<T extends abstract new (...args: never) => unknown> = new (
-  ...params: ConstructorParameters<T>
-) => InstanceType<T>;
-
-type Entries<T> = {
-  [K in keyof T]: [K, T[K]];
-}[keyof T][];
